4a365528a69ee7c044a1f936553106f362331dbf
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-04-27 22:38:33 +0700

    revert: 9101d814dbd5b3dcee2c325500c1a764211ca5de

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const application = require('./route/index.js');
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,10 +17,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     }
 );
-
-app.use('/', application);
