8533f4949ddd84c2f5f0420385f61a2f93c3c6ef
PannnTastic <pannntastic@gmail.com>
2025-04-29 20:22:03 +0700

    pp

--- backend/server/route/laporanRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const laporanController = require('../controllers/laporanController');
-
-// Create Laporan (no login required)
-router.post('/', laporanController.createLaporan);
-
-module.exports = router;
